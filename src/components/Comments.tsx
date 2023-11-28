2913ff68297160e9eee44696f12d92216f2ef912
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-11-28 15:01:30 +0000

    add comments

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 
 export default async function Comments({ postSlug }: { postSlug: string }) {
+  const WEBSITE_URL = "http://localhost:3000";
+
+  let comments = [];
+
+  try {
+    const commentsResult = await fetch(
+      `${WEBSITE_URL}/api/comments/${postSlug}`,
+      { next: { revalidate: 5 } }
+    );
+    const response = await commentsResult.json();
+    console.log(response);
+    comments = response.comments.rows;
+  } catch (err) {
+    console.log(err);
+  }
+
   return (
     <div>
       <h2>| Comments |</h2>
       <h3>Leave a comment: </h3>
 
-      <form action={"/api/comments/${postSlug}"} method="POST">
+      <form action={`/api/comments/${postSlug}`} method="POST">
         <label htmlFor="username">Name:</label>
         <input type="text" name="username" />
 
@@ -20,6 +36,16 @@ export default async function Comments({ postSlug }: { postSlug: string }) {
 
         <button type="submit">Send comment</button>
       </form>
+      {/* @ts-ignore */}
+      {comments.map((comment) => {
+        return (
+          <li key={comment.id}>
+            {comment.username} says...
+            <br />
+            {comment.comment}
+          </li>
+        );
+      })}
     </div>
   );
 }
