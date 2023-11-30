82b9816d2189d6c54a8ad255249f3e9a3f883544
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-11-30 08:40:53 +0000

    add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import Intro from "@/components/Intro";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="">
+      <Intro />
+    </main>
   );
 }
